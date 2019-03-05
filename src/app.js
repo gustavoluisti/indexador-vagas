913cbf5361c8c18b5b8b296ebeb418e1f050ea4c
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2019-03-05 06:03:21 -0300

    Configurando services

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const ENV = process.env.NODE_ENV || 'development'
 const envPath = path.join(__dirname, `./dist/configs/env/${ENV}`)
 
 require(envPath)(app)
+require('./routes/')(app)
 
 app.listen(app.get('port'), ()=> {
     console.log('Express has been started')
--- src/configs/env/development.js
+++ src/configs/env/development.js
@@ -1,5 +1,28 @@
+import path from 'path'
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
+import hbs from 'express-hbs'
+import methodOverride from 'method-override'
+import session from 'express-session'
+
 module.exports = (app) => {
     app.set('port', 9000)
     app.set('views', path.join(__dirname, './../../views'))
     app.set('view engine', 'hbs')
+
+    app.use(morgan('dev'))
+    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.json())
+    app.use(methodOverride('_method'))
+    app.use(session({
+        secret: '#&*#$%@#$$#$%##*&%$#',
+        resave: false,
+        saveUninitialized: false
+    }))
+
+    app.engine('hbs', hbs.express4({
+        defaultLayout: path.join(app.get('views'), 'layouts/main.hbs'),
+        partialsDir  : path.join(app.get('views'), 'partials'),
+        layoutsDir   : path.join(app.get('views'), 'layouts') 
+    }))
 }
